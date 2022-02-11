b39e1d7bd6b167f6fcc32e6c733d05e50d4ee6ec
E-Humphrey <19109@totton.ac.uk>
2022-02-11 12:32:17 +0000

    Unity start
    .

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+
+    public int WeakHP = 2;
+    public int NormHP = 5;
+    public int FastHP = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
