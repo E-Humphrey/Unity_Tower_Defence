8e2b2dc5737ece576c4b3763bb5ab7b0890791c0
E-Humphrey <19109@totton.ac.uk>
2022-02-28 16:22:39 +0000

    new

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+
+    
+
+    private void Start()
+    {
+        Transform[]  CP = GetComponentsInChildren<Transform>();
+
+        var test = 1;
+    }
+
+   
+
+}
